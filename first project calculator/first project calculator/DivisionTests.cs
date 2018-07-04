f7a8af77263dadd83d2f7f090a88a8175d0d2191
DreamwalkerDaria <gudkova2804@gmail.com>
2018-07-04 13:31:07 +0600

    Test Division

---
+++ first project calculator/first project calculator/DivisionTests.cs
@@ -0,0 +1,18 @@
+﻿using first_project_calculator;
+using first_project_calculator.TwoArgument;
+using NUnit.Framework;
+
+namespace first_project_calculator
+{
+    [TestFixture]
+    public class DivisionTests
+    {
+        [Test]
+        public void DivisionCalculatorTests()
+        {
+            ICalculatorTwoArguments calculator = new DivisionCalculator();
+            double actual = calculator.Calculate(2, 2);
+            Assert.AreEqual(1, actual);
+        }
+    }
+}
