cef2972b1fd36dad1236b65ecf52fcdd5e74bb11
DreamwalkerDaria <gudkova2804@gmail.com>
2018-07-04 13:28:36 +0600

    Tests Multiply

---
+++ first project calculator/first project calculator/MultiplyTests.cs
@@ -0,0 +1,18 @@
+﻿using first_project_calculator;
+using first_project_calculator.TwoArgument;
+using NUnit.Framework;
+
+namespace first_project_calculator
+{
+    [TestFixture]
+    public class MultiplyTests
+    {
+        [Test]
+        public void MultiplyCalculatorTests()
+        {
+            ICalculatorTwoArguments calculator = new MultiplyCalculator();
+            double actual = calculator.Calculate(2, 3);
+            Assert.AreEqual(6, actual);
+        }
+    }
+}
