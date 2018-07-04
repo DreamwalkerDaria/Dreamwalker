9a50426a174390f4007b7f06af960646fdc27060
alexeydikun <dikun-alesha@mail.ru>
2018-07-04 21:45:09 +0600

    Root, Degree, Cube HardTests

---
+++ first project calculator/first project calculator/HardTests/CubeHardTest.cs
@@ -0,0 +1,19 @@
+﻿using first_project_calculator.OneArgument;
+using NUnit.Framework;
+
+namespace first_project_calculator.HardTests
+{
+    [TestFixture]
+    public class CubeHardTests
+    {
+        [TestCase(2, arg2: 8)]
+        [TestCase(4, arg2: 64)]
+        [TestCase(5, arg2: 125)]
+        public void CubeCalculatorTests(double firstArgument, double result)
+        {
+            var calculator = new Cube();
+            var actualResult = calculator.Calculate(firstArgument);
+            Assert.AreEqual(result, actualResult);
+        }
+    }
+}
