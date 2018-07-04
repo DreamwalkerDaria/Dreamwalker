b19b7ea0f7e7eb6203371010353edb2837293922
alexeydikun <dikun-alesha@mail.ru>
2018-07-04 21:17:42 +0600

    TwoArgumentTest

---
+++ first project calculator/first project calculator/ArgumentsTests/TwoArgumentTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using first_project_calculator.TwoArgument;
+using NUnit.Framework;
+
+namespace first_project_calculator.ArgumentsTests
+{
+    public class CalculatorTwoArgumentsTests
+    {
+        [TestCase("Addition", typeof(AdditionCalculator))]
+        [TestCase("Subtraction", typeof(SubtractionCalculator))]
+        [TestCase("Multiply", typeof(MultiplyCalculator))]
+        [TestCase("Division", typeof(DivisionCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = CalculateTwoFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
