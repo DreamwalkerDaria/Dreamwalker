818b141fb2772a3ec0009c9ea5d225a1464fc064
DreamwalkerDaria <gudkova2804@gmail.com>
2018-07-04 13:25:01 +0600

    Test Subtraction

---
+++ first project calculator/first project calculator/SubtractionTests.cs
@@ -0,0 +1,18 @@
+﻿using first_project_calculator;
+using first_project_calculator.TwoArgument;
+using NUnit.Framework;
+
+namespace first_project_calculator
+{
+    [TestFixture]
+    public class SubtractionTests
+    {
+        [Test]
+        public void SubtractionCalculatorTests()
+        {
+            ICalculatorTwoArguments calculator = new SubtractionCalculator();
+            double actual = calculator.Calculate(2, 2);
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
