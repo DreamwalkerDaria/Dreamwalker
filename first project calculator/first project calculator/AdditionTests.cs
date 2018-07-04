2bb37628071fb641c1686e7a0888a6a17dc32ebe
DreamwalkerDaria <gudkova2804@gmail.com>
2018-07-04 13:18:02 +0600

    Test Addition

---
+++ first project calculator/first project calculator/AdditionTests.cs
@@ -0,0 +1,18 @@
+﻿using first_project_calculator;
+using first_project_calculator.TwoArgument;
+using NUnit.Framework;
+
+namespace first_project_calculator
+{
+    [TestFixture]
+    public class AdditionTests
+    {
+    [Test]
+    public void AdditionCalculatorTests()
+    {
+    ICalculatorTwoArguments calculator = new AdditionCalculator();
+    double actual = calculator.Calculate(2, 2);
+    Assert.AreEqual(4, actual);
+    }
+    }
+}
