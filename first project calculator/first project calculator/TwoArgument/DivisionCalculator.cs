73f15f9d96934ff263a4f3c1d9707ed715f4194a
DreamwalkerDaria <gudkova2804@gmail.com>
2018-07-04 19:25:03 +0600

    Division 0

--- first project calculator/first project calculator/TwoArgument/DivisionCalculator.cs
+++ first project calculator/first project calculator/TwoArgument/DivisionCalculator.cs
@@ -1,9 +1,15 @@
-﻿namespace first_project_calculator.TwoArgument
+﻿using System;
+
+namespace first_project_calculator.TwoArgument
 {
     public class DivisionCalculator : ICalculatorTwoArguments
     {
         public double Calculate(double firstArgument, double secondArgument)
         {
+            if (secondArgument == 0)
+            {
+                throw new Exception("Деление на 0");
+            }
             return firstArgument / secondArgument;
         }
     }
